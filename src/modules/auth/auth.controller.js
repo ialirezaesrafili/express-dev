ce26ff63aa9effdb8be35c106b30133279719ab0
ialirezaesrafili <alirezaesrafili53@gmail.com>
2024-08-08 06:54:56 -0700

    working on auth controller

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,22 @@
+class AuthController {
+    async sendOTP(req, res, next) {
+        try {
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async checkOTP(req, res, next) {
+        try {
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async logout(req, res, next) {
+        try {
+        } catch (error) {
+            next(error)
+        }
+    }
+}
