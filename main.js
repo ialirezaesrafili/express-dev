b27e4fd6f6a5030e97abb4d1a80b10e4b9649379
ialirezaesrafili <alirezaesrafili53@gmail.com>
2024-08-08 02:39:33 -0700

    modify  package-lock.json and package.json

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 const dotenv = require("dotenv");
 dotenv.config()
 const PORT = process.env.PORT || 3000;
-async function main(){
+
+async function main() {
     const app = express();
     // config
     require("./src/config/mongoos.config");
+    // adding app route
     app.listen(PORT, () => {
         console.log(`[SERVER] is running on ${PORT}`)
     })
